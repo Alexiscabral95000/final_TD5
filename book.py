e428e80a59fda60968e3471006398bce22426697
alexiscabral95000 <cabral.alexis@outlook.fr>
2021-02-28 15:11:20 -0500

    using pandas for better representation

--- book.py
+++ book.py
@@ -1,3 +1,5 @@
+import pandas as pd 
+
 class Book :
 	def __init__(self,Name) :
 		self.name = Name 
@@ -8,8 +10,7 @@ class Book :
 		print("--------------------")
 		print("----- Insert Buy "+str(quantity)+"@"+str(price) + " id=" + str(len(self.orders)) +" on "+self.name)
 		self.orders.append(buy_order)
-		for order in self.orders :
-			print(order)
+		print(self)
 
 		
 	def insert_sell(self,quantity,price) :
@@ -18,9 +19,22 @@ class Book :
 		print("--------------------")
 		print("----- Insert SELL "+str(quantity)+"@"+str(price) + " id=" + str(len(self.orders)) +" on "+self.name)
 		self.orders.append(sell_order)
-		print(self.name)
-		for order in self.orders :
-			print(order)
+		print(self)
+	
+	def __repr__(self) : 
+		df = pd.DataFrame(columns = ["id","type","quantity","unit price","is executed"])
+
+		for order in self.orders : 
+			new_row = {"id" : order.id,
+					   "type" : order.type,
+					   "quantity" : order.quantity,
+					   "unit price" : order.unit_price,
+					   "is executed" : order.is_executed
+					  }
+			df = df.append(new_row,ignore_index = True)
+			
+		string_rep = "BOOK: " + self.name + "\n\n" + df.to_string()
+		return string_rep
 
 class Order:
 	def __init__(self,book_name,id,type,quantity,price,is_executed=False) :
